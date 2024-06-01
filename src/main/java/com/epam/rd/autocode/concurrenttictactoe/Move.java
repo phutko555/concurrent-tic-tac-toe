28bfb4a462e813251dadc18425d0c19db5950e0b
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-06-01 14:23:58 +0000

    Update Move.java

--- src/main/java/com/epam/rd/autocode/concurrenttictactoe/Move.java
+++ src/main/java/com/epam/rd/autocode/concurrenttictactoe/Move.java
@@ -8,4 +8,12 @@ public class Move {
         this.row = row;
         this.column = column;
     }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
 }
